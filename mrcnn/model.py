e0930468773a81783908081226e1649bf4a94fc1
shamanth dinesh thelkar <shamanthdt.sse21@rvce.edu.in>
2023-04-13 23:43:32 +0530

    added

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -79,7 +79,7 @@ def compute_backbone_shapes(config, image_shape):
         return config.COMPUTE_BACKBONE_SHAPE(image_shape)
 
     # Currently supports ResNet only
-    assert config.BACKBONE in ["resnet50", "resnet101"]
+    assert config.BACKBONE in ["resnet50", "VGG16"]
     return np.array(
         [[int(math.ceil(image_shape[0] / stride)),
             int(math.ceil(image_shape[1] / stride))]
